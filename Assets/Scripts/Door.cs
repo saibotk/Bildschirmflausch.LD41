eb8786a30a87b24ed81c9ed20d05d80defaeae45
Jan <Jidbo@users.noreply.github.com>
2018-04-21 16:24:58 +0200

    add change of collision for door, moved player to prefab

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -4,19 +4,24 @@ using UnityEngine;
 
 public class Door : MonoBehaviour {
     private bool locked = false;
+
+    BoxCollider2D boundingBox;
 	// Use this for initialization
 	void Start () {
-        locked = false;
+        boundingBox = GetComponent<BoxCollider2D>();
+        Unlock();
 	}
 	
     public void Lock()
     {
         locked = true;
+        boundingBox.enabled = true;
     }
 
     public void Unlock()
     {
         locked = false;
+        boundingBox.enabled = false;
     }
 
     public bool IsLocked()
