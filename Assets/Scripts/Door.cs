d9b5558af1e087549462183ae2c0324924bad35c
Unknown <saibotk@users.noreply.github.com>
2018-04-21 15:39:48 +0200

    Room and Door

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+    private bool locked = false;
+	// Use this for initialization
+	void Start () {
+        locked = false;
+	}
+	
+    public void Lock()
+    {
+        locked = true;
+    }
+
+    public void Unlock()
+    {
+        locked = false;
+    }
+
+    public bool IsLocked()
+    {
+        return locked;
+    }
+}
--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -2,15 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class Room : MonoBehaviour {
+
+    [SerializeField]
+    int Width, Height;  // Gridsize for Generation
+    List<Door> doors;
+
+    [SerializeField]
+    GameObject doorsRootObject;
+
+    [SerializeField]
+    private Objective objective; 
 
 	// Use this for initialization
 	void Start () {
-		
+        doors = new List<Door>();
+        foreach (Door d in doorsRootObject.GetComponentsInChildren<Door>())
+        {
+            doors.Add(d);
+        }
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void Lock()
+    {
+        foreach (Door d in doors)
+        {
+            d.Lock();
+        }
+    }
+
+    void Unlock()
+    {
+        foreach (Door d in doors)
+        {
+            d.Unlock();
+        }
+    }
+
+
 }
