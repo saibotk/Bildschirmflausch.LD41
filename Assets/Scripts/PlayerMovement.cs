09116b9b543f57e9fce0b06edeb634f7c968dbd5
Piegames <14054505+piegamesde@users.noreply.github.com>
2018-04-21 17:22:29 +0200

    DRIFTING!

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,42 +2,50 @@
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float power = 3;
-    public float turnpower = 2;
-    public float curveSpeed = 0.5f;
-    Vector3 curspeed;
+	[SerializeField]
+	public float acceleration = 3;
+	[SerializeField]
+	public float friction = 0.1f;
+	[SerializeField]
+	public float turnSpeed = 2;
+	[SerializeField]
+	public float drift = 1f;
+
     Rigidbody2D rigidbody2D;
 
     // Use this for initialization
-    void Start()
-    {
+    void Start() {
         rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
-
     void FixedUpdate() {
-        
-        curspeed = new Vector3(rigidbody2D.velocity.x, rigidbody2D.velocity.y, 0);
+		Vector3 speedVec = new Vector3(rigidbody2D.velocity.x, rigidbody2D.velocity.y, 0);
+		float speed = speedVec.magnitude;
 
-        //curspeed = curspeed.normalized;
-        //curspeed *= maxspeed;
-
-        rigidbody2D.AddForce(transform.up * power);
-        Vector3 forwardNorm = (transform.localRotation * Vector3.up).normalized * curspeed.magnitude;
-        Vector3 br = forwardNorm - curspeed;
-        br *= curveSpeed;
-        rigidbody2D.AddForce(br);
-        //Debug.Log(br);
-
+		{ // Forward
+			rigidbody2D.AddForce(transform.up * acceleration);
+        }
+        {// Drag
+			Vector3 drag = speedVec.normalized * speed * speed * friction * -1;
+            rigidbody2D.AddForce(drag);
+            Debug.DrawLine(transform.position, transform.position + drag, Color.cyan, 0.01f, false);
+        }
+        { // Drift
+			Vector3 forwardNorm = (transform.localRotation * Vector3.up).normalized * speedVec.magnitude;
+			Vector3 br = forwardNorm - speedVec;
+			br *= drift;
+			rigidbody2D.AddForce(br);
+			//Debug.Log(br);
+			Debug.DrawLine(transform.position, transform.position + br, Color.red, 0.01f, false);
+        }
         if (Input.GetKey(KeyCode.A)){
-            transform.Rotate(Vector3.forward * turnpower);
+            transform.Rotate(Vector3.forward * turnSpeed);
         }
         if (Input.GetKey(KeyCode.D)) {
-            transform.Rotate(Vector3.forward * -turnpower);
+            transform.Rotate(Vector3.forward * -turnSpeed);
         }
         // Debug lines
-        Debug.DrawLine(transform.position, transform.position + curspeed, Color.magenta, 0.01f, false);
-        Debug.DrawLine(transform.position, transform.position + br, Color.red, 0.01f, false);
+        Debug.DrawLine(transform.position, transform.position + speedVec, Color.magenta, 0.01f, false);
         Debug.DrawLine(transform.position, transform.position + transform.localRotation * Vector3.up, Color.yellow, 0.01f, false);
 
         //Debug.Log(transform.localRotation.eulerAngles);
