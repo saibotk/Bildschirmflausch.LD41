bd9f61a671e9a7717cc2d9ce52e8c809cc3dbde7
ALoTron <34157676+ALoTron@users.noreply.github.com>
2018-04-21 17:27:28 +0200

    Done some Stuff, and some more ...

--- Assets/Scripts/Entities/Mob.cs
+++ Assets/Scripts/Entities/Mob.cs
@@ -2,15 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Mob : MonoBehaviour {
+public abstract class Mob : Entity {
+	readonly int maxHP;
+	int currentHP;
+	bool isDead;
 
-	// Use this for initialization
-	void Start () {
-		
+	// Constructor
+	public Mob(EntityObjective referringObjective, int maxHP) : base(referringObjective)
+	{
+		this.maxHP = maxHP;
+		currentHP = maxHP;
+
+		isDead = false;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// inflicts damage to this mob
+	public void InflictDamage(int damage)
+	{
+		currentHP -= damage;
+		if (!isDead && currentHP <= 0) 
+		{
+			base.Kill ();
+			isDead = true;
+		}
+	}
+
+	// Heals the mob
+	public void Heal(int healAmount)
+	{
+		if (!isDead)
+			currentHP = (currentHP + healAmount > currentHP) ? maxHP : currentHP + healAmount;
 	}
 }
