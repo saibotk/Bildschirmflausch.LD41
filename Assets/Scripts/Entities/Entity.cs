1ef7a241426e09d3714781c4c2d056f3ebb322ff
ALoTron <34157676+ALoTron@users.noreply.github.com>
2018-04-21 16:12:04 +0200

    Added entity-spawn and kill

--- Assets/Scripts/Entities/Entity.cs
+++ Assets/Scripts/Entities/Entity.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Entity : MonoBehaviour {
+public class Entity {
+	EntityObjective referringObjective;
+	GameObject entityPrefab;
+	GameObject instance;
 
-	// Use this for initialization
-	void Start () {
-		
+	// Constructor
+	public Entity(EntityObjective referringObjective, GameObject entityPrefab)
+	{
+		this.referringObjective = referringObjective;
+		this.entityPrefab = entityPrefab;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	// spawns the entity
+	public void Spawn(Transform spawnPoint)
+	{
+		instance = GameObject.Instantiate (entityPrefab);
+		instance.transform = spawnPoint;
+	}
+
+	// kills the entity
+	public void Kill()
+	{
+		GameObject.Destroy (instance);
+		referringObjective.Remove (this);
+
+		instance = null;
 	}
 }
