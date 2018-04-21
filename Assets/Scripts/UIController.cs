c604f6e4e696b121a03a22ca8274674f251c7bc4
Unknown <34045228+sommerlilie@users.noreply.github.com>
2018-04-21 20:47:50 +0200

    Some UI

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -3,31 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class UIController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-    public static UIController instance;
-
-    private GameObject score;
-    private GameObject pauseMenu;
-    private GameObject GameOverPanel;
-
     public UIController()
     {
         UIController.instance = this;
     }
+
+    private GameObject score;
+    private GameObject pauseMenu;
+    private GameObject GameOverPanel;
+    // Use this for initialization
+    void Start () {
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+    public static UIController instance;
+
     public void quitGame()
     {
         Application.Quit();
     }
- 
-    
+    public void ShowPauseMenu()
+    {
+        pauseMenu.SetActive(true);
+    }
+    public void ClosePauseMenu()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+
+
 }
 
