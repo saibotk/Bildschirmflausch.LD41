4fc1be09f9085182630a5a8a3d299d96a2f4465e
ALoTron <34157676+ALoTron@users.noreply.github.com>
2018-04-21 16:23:37 +0200

    Fixed Constructor-error in Collectable

--- Assets/Scripts/Entities/Collectable.cs
+++ Assets/Scripts/Entities/Collectable.cs
@@ -3,5 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Collectable : Entity{
-	
+
+	public Collectable (EntityObjective referringObjective, GameObject entityPrefab) : base(referringObjective, entityPrefab) {}
 }
