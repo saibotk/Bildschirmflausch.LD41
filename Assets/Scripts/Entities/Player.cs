5c30563dcca70af1abc946e6f8fdbd437addea0f
Jan <Jidbo@users.noreply.github.com>
2018-04-21 19:50:12 +0200

    add collision for player

--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -6,7 +6,27 @@ public class Player : Mob {
 
     public Player() : base(null, 100 )
     {
-        
+                
     }
 
+	private void OnCollisionEnter(Collision collision)
+	{
+        Debug.Log("Collision");
+        if (collision.collider.tag == "wall") {
+            Kill();
+        }
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+        if (other.tag == "door") {
+            Debug.Log("Open door");
+        }
+	}
+
+    public override void Kill()
+    {
+        base.Kill();
+        Destroy(this.gameObject);
+    } 
 }
