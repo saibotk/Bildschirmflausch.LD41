788d87452dd97b5f1f8aad1c06f11d29bf83e609
Saibotk <saibotk@users.noreply.github.com>
2018-04-22 23:59:13 +0200

    Collision for bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -26,15 +26,14 @@ public class Bullet : MonoBehaviour {
 	void Update () {
         if ( owner == null )
             return;
-        transform.position = transform.position + transform.localRotation * Vector3.up * speed * Time.deltaTime;
+        transform.position = transform.position + transform.localRotation * Vector3.up * speed * Time.deltaTime; // TODO do with physics (rigidbody)
 	}
 
     void OnCollisionEnter2D(Collision2D collision) {
         if ( owner == null )
             return;
-        Debug.Log("Collided");
+        
         if ( collision.collider.gameObject.tag != owner.tag ) {
-            Debug.Log("calc");
             Mob m = collision.collider.gameObject.GetComponent(typeof(Mob)) as Mob;
             if (m != null) {
                 m.InflictDamage(damage);
