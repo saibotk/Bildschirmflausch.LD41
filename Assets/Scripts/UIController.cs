2b8630b55202b7bfa83d290f44449b04d8daa9ab
Jan <Jidbo@users.noreply.github.com>
2018-04-22 20:10:05 +0200

    fixes to the notification system

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -2,53 +2,74 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UIController : MonoBehaviour {
+public class UIController : MonoBehaviour
+{
 
     GameObject score;
     GameObject pauseMenu;
 
     [SerializeField]
+    NotificationManager notifications;
+
+    [SerializeField]
     GameObject gameOverPanel;
 
     [SerializeField]
-    private HealthbarController healthcontroller;
+    HealthbarController healthcontroller;
 
-    public void ShowPauseMenu() {
+  
+    public void ShowPauseMenu()
+    {
         pauseMenu.SetActive(true);
     }
 
-    public void ClosePauseMenu() {
+    public void ClosePauseMenu()
+    {
         pauseMenu.SetActive(false);
     }
 
-    public void LoadSceneByIndex(int index) {
+    public void LoadSceneByIndex(int index)
+    {
         Debug.Log("Loaded scene " + index);
         UnityEngine.SceneManagement.SceneManager.LoadScene(index);
     }
 
-    public void QuitGame() {
+    public void QuitGame()
+    {
         Debug.Log("Quit game");
         Application.Quit();
     }
 
-    public void ShowGameOverUI() {
-        if ( gameOverPanel != null ) {
+    public void ShowGameOverUI()
+    {
+        if (gameOverPanel != null)
+        {
             Canvas gameOverCanvas = gameOverPanel.GetComponent<Canvas>();
             Debug.Log("Loading Canvas");
 
-            if ( gameOverCanvas != null ) {
+            if (gameOverCanvas != null)
+            {
                 Debug.Log("Loaded Canvas");
                 gameOverCanvas.enabled = true;
-            } else {
+            }
+            else
+            {
                 Debug.Log("Gameover panel has no Canvas");
             }
-        } else {
+        }
+        else
+        {
             Debug.Log("No game over panel assigned");
         }
     }
 
-    public void InitHealthController(Player ply) {
+    public void InitHealthController(Player ply)
+    {
         healthcontroller.SetPlayer(ply);
+    }
+
+    public NotificationManager GetNotificationManager() {
+        return notifications;
     }
 }
 
