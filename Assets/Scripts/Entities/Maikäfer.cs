18ec2c326fc64c22f4240ba582299f1230f8bb57
Unknown <34045228+sommerlilie@users.noreply.github.com>
2018-04-23 16:08:16 +0200

    Maikäfer + Spider

---
+++ Assets/Scripts/Entities/Maikäfer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Entities.Attack;
+
+namespace Assets.Scripts.Entities { 
+
+public class Maikäfer : MonoBehaviourEnemy {
+
+        public Maikäfer() : base(15)
+        {
+
+        }
+
+        private void Start()
+        {
+            SetAttack(new MeleeAttack(this.gameObject));
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Spider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Entities.Attack;
+
+namespace Assets.Scripts.Entities
+{
+    class Spider : Enemy
+    {
+        public Spider() : base(45)
+        {
+
+        }
+
+        private void Start()
+        {
+            SetAttack(new SingleShot(this.gameObject));
+        }
+    }
+}
