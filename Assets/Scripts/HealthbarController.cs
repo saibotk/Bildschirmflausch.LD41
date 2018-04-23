8843cde6508a1f040c283e39b39a6cd4bba0d8aa
Jan <Jidbo@users.noreply.github.com>
2018-04-23 21:47:29 +0200

    fixed HealthBar

--- Assets/Scripts/HealthbarController.cs
+++ Assets/Scripts/HealthbarController.cs
@@ -6,26 +6,36 @@ public class HealthbarController : MonoBehaviour {
 
     float currentRotation;
     float maxRotation;
-    private Player player;
+    Player player;
 
     // Update is called once per frame
     void Update()
     {
         // if player alive and spawned
         if (player != null)
-        {
+        {         
+            Debug.Log(player.GetHealth());
             UpdatePointer(player.GetHealth());
         }
         else
         {
             //if player dead or not spawned
             UpdatePointer(0);
-
         }
     }
 
     void UpdatePointer(float playerLife) {
-        float offset = playerLife - currentRotation;
+        float offset;
+
+        if (player == null) {
+            currentRotation = 0;
+            offset = 0;
+            Debug.Log("Player not found");
+        } else {
+            Debug.Log("calculated offset");
+            offset = ((playerLife / maxRotation) * 100) - currentRotation;
+        }
+
         gameObject.transform.Rotate(Vector3.forward, offset);
         currentRotation += offset;
     }
@@ -33,6 +43,7 @@ public class HealthbarController : MonoBehaviour {
     public void SetPlayer(Player ply) {
         player = ply;
         maxRotation = player.GetMaxHealth();
-        currentRotation = player.GetHealth();
+        //currentRotation = (player.GetHealth() / maxRotation) * 100;
+        Debug.Log("Set Player");
     }
 }
