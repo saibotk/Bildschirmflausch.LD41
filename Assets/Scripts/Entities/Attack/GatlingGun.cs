e81f7808d7aa8617b918bdeb28bf535e37efddab
Saibotk <saibotk@users.noreply.github.com>
2018-04-23 18:38:43 +0200

    GATLING GUN

---
+++ Assets/Scripts/Entities/Attack/GatlingGun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Assets.Scripts.Entities.Attack {
+    class GatlingGun : SingleShot {
+        public GatlingGun(GameObject owner) : base(owner) {
+            damage = 5;
+            cooldown = 0.1f;
+            speed = 20;
+        }
+
+    }
+}
--- Assets/Scripts/Entities/Player.cs
+++ Assets/Scripts/Entities/Player.cs
@@ -10,6 +10,9 @@ public class Player : Mob {
     [SerializeField]
     private int carDamage = 5;
 
+    private SingleShot singleShot;
+    private GatlingGun ggun;
+
     private float nextAttackTime;
 
     public Player() : base(100) { }
@@ -18,10 +21,25 @@ public class Player : Mob {
         SingleShot s = new SingleShot(this.gameObject);
         s.SetPrefab(bulletPrefab);
         s.SetSpawn(bulletSpawn);
+        singleShot = s;
+        GatlingGun g = new GatlingGun(this.gameObject);
+        g.SetPrefab(bulletPrefab);
+        g.SetSpawn(bulletSpawn);
+        ggun = g;
         SetAttack(s);
     }
 
     void Update() {
+        if(Input.GetKeyDown(KeyCode.G)) {
+            if(attack != ggun) {
+                attack = ggun;
+                Debug.Log("Switched to GatlingGun");
+            } else {
+                attack = singleShot;
+                Debug.Log("Switched to SingleShot");
+            }
+            
+        }
         if ( Time.timeSinceLevelLoad >= nextAttackTime && attack != null) {
             if ( Input.GetAxis("Fire") > 0 ) {
                 Debug.Log("Attack pressed!");
