3fd5637ec92ec9cb5fddc1531bc9580cf1cafbf3
Unknown <Himusoka>
2018-04-23 02:37:57 +0200

    Added main(0.5) + some AudioControl

--- Assets/Scripts/AudioControl.cs
+++ Assets/Scripts/AudioControl.cs
@@ -6,22 +6,47 @@ using UnityEngine.Audio;
 public class AudioControl : MonoBehaviour
 {
     public AudioMixer mixer;
-    public AudioMixerSnapshot maintheme;
-    public AudioMixerSnapshot end;
+    public AudioSource maintheme;
     public AudioSource gameovers;
     public AudioSource[] soundeffects;
 
-    public enum Sfx { shoot, explosion, mobattack};
+    public enum Sfx { shoot, explosion, mobattack };
 
-    private float m_TransitionIn;
-    private float m_TransitionOut;
-    private bool endstate;
+    private const float lvlbgm = 37.879f;
+    private bool menu;
 
     // Use this for initialization
     void Start()
     {
-        m_TransitionIn = 1.2f;
-        m_TransitionOut = 0.3f;
+        maintheme.loop = true;
+        gameovers.loop = true;
+        maintheme.Play();
+    }
+
+    private void Update()
+    {
+        if (maintheme.time >= 99.0f)
+            maintheme.time = lvlbgm;
+        if (gameovers.time >= 22.85f)
+            gameovers.time = 3;
+    }
+
+    public void GameOverBgm()
+    {
+        maintheme.Stop();
+        gameovers.Play();
+    }
+
+    public void MenuBgm()
+    {
+        if (gameovers.isPlaying)
+            gameovers.Stop();
+        maintheme.Play();
+    }
+
+    public void LevelBgm()
+    {
+        maintheme.time = lvlbgm;
     }
 
     public void SfxPlay(int sound)
@@ -42,4 +67,15 @@ public class AudioControl : MonoBehaviour
     {
         mixer.SetFloat("masterVolume", Mathf.Clamp(nvol, -80f, 20f));
     }
+
+    public void SetBgmVolume(float nvol)
+    {
+        mixer.SetFloat("bgmVolume", Mathf.Clamp(nvol, -80f, 20f));
+    }
+
+    public void SetSfxVolume(float nvol)
+    {
+        mixer.SetFloat("sfxVolume", Mathf.Clamp(nvol, -80f, 20f));
+    }
 }
+
